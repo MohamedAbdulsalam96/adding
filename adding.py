75532375978675e57c3bc5e092b20ab7c4fcc738
Mohamed_Abdulsalam <moha2016it@gmail.com>
2022-01-13 13:41:07 +0300

    adding

---
+++ adding.py
@@ -0,0 +1,11 @@
+# #hi! My name is: Mohamed Abdulsalam..
+#practice cource..
+# #challeange (6.1)
+"""
+Write sub program add two or three numbers? upload the code to GitHub,
+call will be like that *
+"""
+def adding (a,b,c=0):
+    return a+b+c
+
+print(adding(5,2))
